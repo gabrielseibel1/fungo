0422973cb743073afbc6fc761ec53737f4ade789
gabrielseibel1 <gabrielseibel1@gmail.com>
2023-08-24 21:01:38 -0300

    Add filter map by key

--- fungomap/filter.go
+++ fungomap/filter.go
@@ -1,5 +1,15 @@
 package fungomap
 
+func FilteredByK[T comparable, U any](m map[T]U, f func(T) bool) map[T]U {
+	r := make(map[T]U)
+	for k, v := range m {
+		if f(k) {
+			r[k] = v
+		}
+	}
+	return r
+}
+
 func FilteredByV[T comparable, U any](m map[T]U, f func(U) bool) map[T]U {
 	r := make(map[T]U)
 	for k, v := range m {
